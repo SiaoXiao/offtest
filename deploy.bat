0de0e2579e5eac7c8c2bd607460aec6f0de0a28d
KeithLee <keith.lee@yaudian.com>
2021-10-22 17:47:16 +0800

    wryyy

--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,13 @@
 
 cd C:\Users\keith.lee\Desktop\test\hello-world
 REM 喔
-git add .
+git add . || goto :error
 
 @REM REM 喔喔
-git commit -m "wryyy"
+git commit -m "wryyy" || goto :error
 
 @REM REM 喔喔喔
-git push orgin main
+git push orgin main || goto :error
 
+:error
+exit /b %errorlevel%
